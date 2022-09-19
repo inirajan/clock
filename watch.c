71d6c0d8c3cf43681a0cd2c8acab9d47b66ac8e9
nirajan rokaya <109614985+inirajan@users.noreply.github.com>
2022-09-19 19:33:26 +0545

    Add files via upload

---
+++ watch.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+
+int main(){
+    int h,m,s; // h is for hour ,m is for minute and s is for second
+    int d=1000; // delay of 10000 mili second
+    printf("\n Set time:");
+    scanf("%d%d%d",&h,&m,&s);
+    if(h<12 || m<60 || s<60){
+        
+        printf(" ERROR ! \n");
+        exit("0");
+    }
+    while(1){                 // ffor infnity loop
+        s++;
+        if(s>59){
+            m++;
+            s=0;
+
+        }
+        if(m>59){
+            h++;
+            m=0;
+
+        }
+        if(h>12){
+            h=1;
+        }
+
+      printf("\n clock :");
+      printf(" %02d : %02d : %02d ",h,m,s);
+      Sleep(d);
+      system("cls");
+
+    }
+
+
+}
+
